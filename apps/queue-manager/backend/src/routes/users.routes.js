115a81e0c9d980f79387baeb119a9c96bc536d36
korinteksarl-gif <korinteksarl@gmail.com>
2026-07-29 01:48:16 +0000

    Update users.routes.js

--- apps/queue-manager/backend/src/routes/users.routes.js
+++ apps/queue-manager/backend/src/routes/users.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { list, updateRole, toggleActive } = require('../controllers/users.controller');
+const { list, updateRole, toggleActive, remove } = require('../controllers/users.controller');
 const { authenticate, requireRole } = require('../middleware/auth');
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.use(authenticate, requireRole(['SUPER_ADMIN']));
 router.get('/', list);
 router.put('/:id/role', updateRole);
 router.put('/:id/active', toggleActive);
+router.delete('/:id', remove);
 
 module.exports = router;
